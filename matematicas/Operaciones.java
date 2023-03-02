e84384465576be2343d8090e6774b643fe72496a
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-03-02 12:29:16 +0100

    Añadidas funciones resta y dec
    Resta: Devuelve la resta de dos números
    Dec: Devuelve el decremento en una unidad de un valor dado

--- matematicas/Operaciones.java
+++ matematicas/Operaciones.java
@@ -47,4 +47,52 @@ public class Operaciones {
     public static int inc(int a) {
         return ((int) inc((long) a));
     }
+
+	
+    /**
+     * resta
+     * Devuelve la resta de dos números
+     * 
+     * @param a Minuendo
+     * @param b Sustraendo
+     * @return La resta de los dos números
+     */
+    public static long resta(long a, long b) {
+        return (suma(a, -b));
+    }
+
+    /**
+     * resta
+     * Devuelve la resta de dos números
+     * 
+     * @param a Minuendo
+     * @param b Sustraendo
+     * @return La resta de los dos números
+     */
+    public static int resta(int a, int b) {
+        return ((int) resta((long) a, (long) b));
+    }
+
+    /**
+     * dec
+     * Decrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a decrementar
+     * @return El valor recibido decrementado en uno
+     */
+    public static long dec(long a) {
+        return (resta(a, 1));
+    }
+
+    /**
+     * dec
+     * Decrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a decrementar
+     * @return El valor recibido decrementado en uno
+     */
+    public static int dec(int a) {
+        return ((int) dec((long) a));
+    }
+
 }
